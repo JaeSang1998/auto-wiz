91aa86a703b469151e517f07432c22ccf8dbebad
jaesang1998 <tunaonchess01@gmail.com>
2026-01-11 17:43:21 +0900

    fix(core): export run result types & bump minor version (1.1.3)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,8 @@
 export * from "./types";
-export { type Flow, type Step, type ElementLocator } from "./types";
+
 export * from "./steps/stepValidation";
 export * from "./storage/flowStorage";
 export * from "./runner";
+
+export { type Flow, type Step, type ElementLocator } from "./types";
+export { type RunResult } from "./runner";
